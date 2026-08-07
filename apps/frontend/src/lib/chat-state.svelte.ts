d771a89867fc714d6a738ea5262b921c8107f451
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2026-08-07 14:11:06 +0200

    fix: first channel should be text-based

--- apps/frontend/src/lib/chat-state.svelte.ts
+++ apps/frontend/src/lib/chat-state.svelte.ts
@@ -796,7 +796,7 @@ class ChatState {
   }
 
   private firstChannelForServer(serverId?: string) {
-    return serverId ? this.channelsByServer[serverId]?.[0]?.channels[0] : null;
+    return serverId ? this.channelsByServer[serverId]?.[0]?.channels.filter(c => c.type === 'TEXT')[0] : null;
   }
 
   private selectInitialChannel() {
